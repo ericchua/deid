0eae3a9f66244623a5e6e55b6286711930899b54
ericchua <eric.chua@singaporetech.edu.sg>
2021-08-06 00:17:36 +0800

    Add files via upload

---
+++ try_dicom2jpg.py
@@ -0,0 +1,20 @@
+﻿# dicom2jpg
+
+import dicom2jpg
+
+
+# convert single DICOM file to jpg format
+
+dicom_img_01 = "D:/deid/Images/Test/DICOM/1-089.dcm"
+
+dicom2jpg.dicom2jpg(dicom_img_01)
+
+
+# convert all DICOM files in dicom_dir folder to png format, and save to op_dir
+# exported filepath is target_root / Today / PatientID_Filetype / StudyDate_StudyTime_Modality_AccNum / Ser_Img.Filetype
+
+dicom_dir = "D:/deid/Images/Test/DICOM/Anonymized_20210725"
+
+op_dir = "D:/deid/Images/Test/PNG"
+
+dicom2jpg.dicom2png(dicom_dir, op_dir)
